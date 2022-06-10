5bff3bfce12c4a222609f4bafdcf25c2c690f98d
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-10 17:45:00 -0600

    Test: #percent_to_integer model method

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe BulkDiscount do
   describe 'relationships' do
     it { should belong_to(:merchant) }
   end
+
+  describe 'instance methods' do
+    it '#percent_to_integer' do
+      merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+      bulk1 = merchant1.bulk_discounts.create!(name: '2 at 20%', percent_discount: 0.2, threshold: 2)
+
+      expect(bulk1.percent_to_integer).to eq(20)
+    end
+  end
 end
