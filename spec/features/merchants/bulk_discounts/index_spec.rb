0b9d71f3115fde7a2ef6d1a38f6581111fb23d65
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-11 13:19:30 -0600

    Test: Button to destory discount

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -43,4 +43,29 @@ RSpec.describe 'Bulk Discounts Index Page' do
     click_link("Create Discount")
     expect(current_path).to eq(new_merchant_bulk_discount_path(merchant1))
   end
+
+  it "has a link to delete a discount" do
+    merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+    bulk1 = merchant1.bulk_discounts.create!(name: '2 at 20%', percent_discount: 0.2, threshold: 2)
+    bulk2 = merchant1.bulk_discounts.create!(name: '5 at 30%', percent_discount: 0.3, threshold: 5)
+
+    visit merchant_bulk_discounts_path(merchant1)
+
+    within "#discount-#{bulk1.id}" do
+      expect(page).to have_button("Delete Discount")
+      click_button("Delete Discount")
+    end
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(merchant1))
+
+    within ".all_discounts" do
+      expect(page).to_not have_content("2 at 20%")
+      expect(page).to_not have_content("Percent Discounted: 20%")
+      expect(page).to_not have_content("Number of Items Required: 2")
+
+      expect(page).to have_content("5 at 30%")
+      expect(page).to have_content("Percent Discounted: 30%")
+      expect(page).to have_content("Number of Items Required: 5")
+    end
+  end
 end
