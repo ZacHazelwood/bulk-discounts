d4a74877e03892d228c735b1d21cee3d08f30084
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-11 14:42:17 -0600

    Test: Link to Update Discount Page

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe 'Bulk Discounts Show Page' do
     expect(page).to_not have_content("Percent Discounted: 100%")
     expect(page).to_not have_content("Number of Items Required: 1")
   end
+
+  it "displays a link to update this discount" do
+    merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+    bulk1 = merchant1.bulk_discounts.create!(name: '2 at 20%', percent_discount: 0.2, threshold: 2)
+
+    visit merchant_bulk_discount_path(merchant1, bulk1)
+
+    expect(page).to have_content("Discount Details")
+    expect(page).to have_content("2 at 20%")
+    expect(page).to have_content("Percent Discounted: 20%")
+    expect(page).to have_content("Number of Items Required: 2")
+  end
 end
