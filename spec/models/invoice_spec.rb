c7b5e6930bb27ae31950f9e67800d1185718c685
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-12 13:42:36 -0600

    Test: #invoice_discount, #discounted_revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -74,7 +74,18 @@ RSpec.describe Invoice do
     it "#total_revenue" do
       expect(invoice1.total_revenue).to eq(68175)
     end
+
+    it "#invoice_discount" do
+      discount = merchant1.bulk_discounts.create!(name: '5 at 50%', percent_discount: 0.5, threshold: 5)
+
+      expect(invoice1.invoice_discount).to eq(34087.5)
+    end
+
+    it "#discounted_revenue" do
+      discount = merchant1.bulk_discounts.create!(name: '3 at 30%', percent_discount: 0.3, threshold: 3)
+
+      expect(invoice1.discounted_revenue).to eq(47722.5)
+      expect(invoice3.discounted_revenue).to eq(195288.8)
+    end
   end
-
-
 end
