ebac992439955711c0fb5d291a54bb04d251b964
Zachary Hazelwood <zhaze001@gmail.com>
2022-06-13 14:47:11 -0600

    Test: Bulk Discount Index lists 3 upcoming holidays

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe 'Bulk Discounts Index Page' do
     expect(current_path).to eq(merchant_bulk_discount_path(merchant1, bulk1))
   end
 
-  it "has a link to create a new discount" do
+  it "has a link to create a new discount", :vcr do
     merchant1 = Merchant.create!(name: "Schroeder-Jerde")
     bulk1 = merchant1.bulk_discounts.create!(name: '2 at 20%', percent_discount: 0.2, threshold: 2)
     bulk2 = merchant1.bulk_discounts.create!(name: '5 at 30%', percent_discount: 0.3, threshold: 5)
@@ -44,7 +44,7 @@ RSpec.describe 'Bulk Discounts Index Page' do
     expect(current_path).to eq(new_merchant_bulk_discount_path(merchant1))
   end
 
-  it "has a link to delete a discount" do
+  it "has a link to delete a discount", :vcr do
     merchant1 = Merchant.create!(name: "Schroeder-Jerde")
     bulk1 = merchant1.bulk_discounts.create!(name: '2 at 20%', percent_discount: 0.2, threshold: 2)
     bulk2 = merchant1.bulk_discounts.create!(name: '5 at 30%', percent_discount: 0.3, threshold: 5)
@@ -66,6 +66,21 @@ RSpec.describe 'Bulk Discounts Index Page' do
       expect(page).to have_content("5 at 30%")
       expect(page).to have_content("Percent Discounted: 30%")
       expect(page).to have_content("Number of Items Required: 5")
+    end
+  end
+
+  it 'should have a section of upcoming holidays', :vcr do
+    merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+    bulk1 = merchant1.bulk_discounts.create!(name: '2 at 20%', percent_discount: 0.2, threshold: 2)
+    bulk2 = merchant1.bulk_discounts.create!(name: '5 at 30%', percent_discount: 0.3, threshold: 5)
+
+    visit merchant_bulk_discounts_path(merchant1)
+
+    within '#holidays' do
+      expect(page).to have_content("Upcoming Holidays")
+      expect(page).to have_content('Juneteenth')
+      expect(page).to have_content('Independence Day')
+      expect(page).to have_content('Labour Day')
     end
   end
 end
